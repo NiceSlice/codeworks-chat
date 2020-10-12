03fd3a45c7d83bcf49edf6c9a931aef207ea0669
niceSlice <tanjamiskovic1@gmail.com>
2020-10-12 17:03:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let messages = [];
+
+function newMessage(text)
+{
+    // time
+    // text
+    // recieved / sent
+}
+
+function send()
+{
+    
+    console.log($('#message-input'));
+}
+
+$( document ).ready(function() {
+
+    $('#layout').scrollTop($('#layout')[0].scrollHeight);
+
+    $('#message-wrapper').append('text');
+
+});
