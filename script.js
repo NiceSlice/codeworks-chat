61d0e5d3ab21157c0b807719dc4d09d60d4bd199
niceSlice <tanjamiskovic1@gmail.com>
2020-10-13 16:59:27 +0100

    second commit

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-let messages = [];
+let n = 0;
 
-function newMessage(text)
+
+function post(text, sent=true)
 {
-    // time
-    // text
-    // recieved / sent
-}
+    let d = new Date();
+    let timestamp = d.getHours() + ":" + d.getMinutes();
 
-function send()
-{
-    
-    console.log($('#message-input'));
-}
-
-$( document ).ready(function() {
+    $('#messages-wrapper').append( $('<div id="message-' + n.toString() + 
+                                    '" class="message message_' + (sent ? 'sent' : 'received') + 
+                                    '"> <span class="message-text">' + 
+                                    text + 
+                                    '</span><span class="message-timestamp">' + 
+                                    timestamp + 
+                                    '</span><div class="arrow arrow_' + 
+                                    (sent ? 'sent' : 'received') + 
+                                    '"></div></div>') );
+    n++;
 
     $('#layout').scrollTop($('#layout')[0].scrollHeight);
+}
 
-    $('#message-wrapper').append('text');
+function onsend()
+{
+    let text = $('#message-input')[0].value;
+
+    if (text === '' || !(/\S/.test(text)))
+    {
+        return;
+    }
+
+    $('#message-input')[0].value = '';
+    
+    post(text);
+    post(text, false);
+}
+
+$(document).on('keydown', function(e)
+{
+    if (e.which == '13' && e.shiftKey === false) { onsend(); }
+})
+
+$(document).ready(function()
+{
 
 });
