e2bca1ee6830278bb4f8a22186778886b13d91dd
niceSlice <tanjamiskovic1@gmail.com>
2020-10-14 13:15:01 +0100

    added typing animation

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+function animation(pace, size1, size2)
+{
+    setTimeout(
+        function(){
+            $('#dot-1').css({'height': size2, 'width': size2});
+
+            setTimeout(
+                function(){
+                    $('#dot-1').css({'height': size1, 'width': size1});
+                    $('#dot-2').css({'height': size2, 'width': size2});
+
+                    setTimeout(
+                        function(){
+                            $('#dot-2').css({'height': size1, 'width': size1});
+                            $('#dot-3').css({'height': size2, 'width': size2});
+
+                            setTimeout(
+                                function(){
+                                    $('#dot-3').css({'height': size1, 'width': size1});
+                                }, pace);
+                        }, pace);
+                }, pace);
+        }, pace);
+}
+
+
+function typing(text)
+{
+    let pace = 300;
+    let size1 = $('.dot').css('height');
+    let size2 = $('.dot-wrapper').css('height');
+    
+
+    let n = Math.min((Math.ceil(text.length / 30)), 5);
+    let i = -1;
+
+    let int = setInterval(function()
+    {
+        $('#typing-bubble').css('display', 'flex');
+
+        animation(pace, size1, size2);
+        i++;
+
+        if(i === n)
+        {
+            $('#typing-bubble').css('display', 'none');
+            post(text, false);
+            clearInterval(int);
+        }
+    },
+    pace*4);
+    
+}
+
+
+
 function post(text, sent=true)
 {
     let d = new Date();
@@ -15,6 +71,8 @@ function post(text, sent=true)
     $('#layout').scrollTop($('#layout')[0].scrollHeight);
 }
 
+let promise;
+
 function onsend()
 {
     let text = $('#message-input')[0].value;
@@ -27,7 +85,7 @@ function onsend()
     $('#message-input')[0].value = '';
     
     post(text);
-    post(text, false);
+    typing(text);
 }
 
 $(document).on('keydown', function(e)
